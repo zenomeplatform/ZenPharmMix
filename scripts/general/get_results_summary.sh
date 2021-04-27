56647bd6285f875f47196a3a870e5d073ac72e75
David Twesigomwe <david@cream-ce.core.wits.ac.za>
2021-04-27 19:51:53 +0200

    fixed minor bug

--- scripts/general/get_results_summary.sh
+++ scripts/general/get_results_summary.sh
@@ -16,21 +16,18 @@ done
 for i in $(cat $FILE1); do 
 
     if [[ $(grep "Possible" ${i}*alleles) && $(grep "*" ${i}*alleles) ]]; then
-    paste <(echo ${i}) <(grep "*" ${i}*alleles) <(grep "Possible" ${i}*alleles) | column -s $'\t' -t >> $FILE2
+    paste <(echo ${i}) <(grep "*" ${i}*alleles) <(grep "Possible" ${i}*alleles) | column -s $'\t' -t -o $'\t' >> $FILE2
 
 
     elif [[ $(grep "*" ${i}*alleles) ]]; then
-    paste <(echo ${i}) <(grep "*" ${i}*alleles) | column -s $'\t' -t >> $FILE2
+    paste <(echo ${i}) <(grep "*" ${i}*alleles) | column -s $'\t' -t -o $'\t' >> $FILE2
     
 
     elif [[ $(grep "Possible" ${i}*alleles) ]]; then
-    paste <(echo ${i}) <(grep "Possible" ${i}*alleles) | column -s $'\t' -t >> $FILE2
+    paste <(echo ${i}) <(grep "Possible" ${i}*alleles) | column -s $'\t' -t -o $'\t' >> $FILE2
 
     else
-    paste <(echo ${i}) <(echo check) | column -s $'\t' -t >> $FILE2
+    paste <(echo ${i}) <(echo check) | column -s $'\t' -t -o $'\t' >> $FILE2
     fi
 
 done
-
-
-
