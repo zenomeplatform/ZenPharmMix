c52a93fcdb74207d5772f2f22eca84d8ca802564
TonyLupara <91198710+TonyLupara@users.noreply.github.com>
2023-04-20 18:59:50 +0300

    Update bcftools_pipe.sh

--- bcftools_pipe.sh
+++ bcftools_pipe.sh
@@ -1,1 +1,1 @@
-bcftools norm -m - ${1} | bcftools view -e 'GT="1/0"' | bcftools view -e 'GT="0/0"' | bcftools view -e 'QUAL<30 || 0<ABHet<0.25' | bgzip -c > ${2}
+bcftools norm -m - ${1} | bcftools view -e 'GT="1/0"' | bcftools view -e 'GT="0/0"' | bcftools view -e 'FILTER="PASS" & INFO/QD<10 || 0<ABHet<0.25' | bgzip -c > ${2}
